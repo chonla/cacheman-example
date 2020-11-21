c094d09d7d16d9479ce52c2a015ab39bb1875f2d
Chonlasith Jucksriporn <chonlasith@gmail.com>
2020-11-21 12:31:48 +0700

    Add sample for echo 3.x and echo 4.x

---
+++ v4/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/chonla/cacheman"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	cacheConfig := &cacheman.Config{
+		Enabled: true,
+		Verbose: true,
+		AdditionalHeaders: map[string]string{
+			"X-Cache":         "true",
+			"X-Cache-Manager": "cacheman",
+		},
+		Paths: []string{
+			"/",
+			"/user/:name",
+		},
+	}
+	cache, err := cacheman.NewBigCache(cacheConfig)
+	if err == nil {
+		e.Use(cacheman.MiddlewareV4(cacheConfig, cache))
+	} else {
+		e.Logger.Error(err.Error())
+	}
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.GET("/user/:name", func(c echo.Context) error {
+		name := c.Param("name")
+		return c.String(http.StatusOK, fmt.Sprintf("Hello, %s!", name))
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
